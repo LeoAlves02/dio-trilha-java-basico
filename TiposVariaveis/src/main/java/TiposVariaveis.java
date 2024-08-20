936f63356081a19a9db01f1c0b06849860a8a32e
Leonardo Alves <78911425+LeoAlves02@users.noreply.github.com>
2024-08-19 22:14:17 -0300

    Add files via upload
    Tipos e Variáveis - Completo

--- TiposVariaveis/src/main/java/TiposVariaveis.java
+++ TiposVariaveis/src/main/java/TiposVariaveis.java
@@ -1,9 +1,26 @@
 public class TiposVariaveis {
 
-    public TiposVariaveis() {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 1400.00;
+
+        /* Tipagem */
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        /* Variáveis e Constantes */
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
-
-
-
 }
+
